f2ca65b24dedcf8122f9da54c736968bdcaa4676
Lucas <lkm027@gmail.com>
2018-01-12 17:25:46 -0500

    Uncommented out code to allow for sentence searches for keywords to work again.]

--- search_for_phrases.py
+++ search_for_phrases.py
@@ -1,28 +1,28 @@
 import emoji
 import send_message
 
-# def check_for_keywords( phrase ):
-#     phrase = phrase.lower()
-#     if "meeting" in phrase:
-#         print_meeting_capture()
-#     if "taylor" in phrase:
-#         print_taylor_capture()
-#     if "salt" in phrase or "salty" in phrase:
-#         print_salt_capture()
-#     if "poop" in phrase:
-#         print_poop_capture()
+def check_for_keywords( phrase ):
+    phrase = phrase.lower()
+    if "meeting" in phrase:
+        print_meeting_capture()
+    if "taylor" in phrase:
+        print_taylor_capture()
+    if "salt" in phrase or "salty" in phrase:
+        print_salt_capture()
+    if "poop" in phrase:
+        print_poop_capture()
 
-# def print_meeting_capture():
-#     send_message( emoji.emojize( ":rose::beer_mug::cactus:\n:rose::beer_mug::cactus:", use_aliases = True ) )
+def print_meeting_capture():
+    send_message( emoji.emojize( ":rose::beer_mug::cactus:\n:rose::beer_mug::cactus:", use_aliases = True ) )
 
-# def print_taylor_capture():
-#     send_message( emoji.emojize( "Actually it's\n:sparkles::cocktail_glass: Tequila Holiday :cocktail_glass::sparkles:", use_aliases = True ) )
+def print_taylor_capture():
+    send_message( emoji.emojize( "Actually it's\n:sparkles::cocktail_glass: Tequila Holiday :cocktail_glass::sparkles:", use_aliases = True ) )
 
-# def print_salt_capture():
-#     send_message( "https://media.giphy.com/media/3o7P4F86TAI9Kz7XYk/giphy.gif" )
+def print_salt_capture():
+    send_message( "https://media.giphy.com/media/3o7P4F86TAI9Kz7XYk/giphy.gif" )
 
-# def print_poop_capture():
-#     send_message( emoji.emojize( ":pile_of_poo: spoopy :pile_of_poo:" ) )
+def print_poop_capture():
+    send_message( emoji.emojize( ":pile_of_poo: spoopy :pile_of_poo:" ) )
 
 def check_if_i_am_is_used_and_get_position( phrase ):
     words = str.split( phrase )
