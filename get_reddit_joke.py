ced680c161caa50a0bec32bfeb604b52cd64a4b7
Lucas <lkm027@gmail.com>
2018-03-29 19:44:00 -0400

    Fix missing comma

--- get_reddit_joke.py
+++ get_reddit_joke.py
@@ -6,7 +6,7 @@ import os
 from send_message import send_message
 
 def get_joke_and_send():
-    reddit = praw.Reddit( client_id = os.getenv( 'REDDIT_CLIENT_ID' )
+    reddit = praw.Reddit( client_id = os.getenv( 'REDDIT_CLIENT_ID' ),
                             client_secret = os.getenv( 'REDDIT_CLIENT_SECRET' ),
                             user_agent = os.getenv( 'REDDIT_USER_AGENT' ) )
 
