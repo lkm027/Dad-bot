fe29109104b9dbaf5cfa36be62fd12aa4bc03440
Lucas <lkm027@gmail.com>
2018-03-29 20:51:43 -0400

    Testing length of joke message

--- get_reddit_joke.py
+++ get_reddit_joke.py
@@ -5,6 +5,8 @@ import os
 
 from send_message import send_message
 
+value = False
+
 def get_joke_and_send():
     reddit = praw.Reddit( client_id = os.getenv( 'REDDIT_CLIENT_ID' ),
                             client_secret = os.getenv( 'REDDIT_CLIENT_SECRET' ),
@@ -14,10 +16,16 @@ def get_joke_and_send():
     for submission in reddit.subreddit( 'jokes' ).hot( limit=50 ):
         if not submission.distinguished:
             jokes.append( submission )
+        if "Daniel" in submission.title and not value:
+            send_joke()
 
     joke = random.choice( jokes )
 
     send_joke( joke )
 
 def send_joke( joke ):
-    send_message( "{}\n\n{}".format( joke.title, joke.selftext ) )
+    try:
+        send_message( "{}\n\n{}".format( joke.title, joke.selftext ) )
+    except:
+        value = True
+        get_joke_and_send()
