f887ca95565c502e00a1629f11582cf4abd509b9
Lucas <lkm027@gmail.com>
2018-03-29 20:48:23 -0400

    Testing length of joke message

--- get_reddit_joke.py
+++ get_reddit_joke.py
@@ -14,13 +14,10 @@ def get_joke_and_send():
     for submission in reddit.subreddit( 'jokes' ).hot( limit=50 ):
         if not submission.distinguished:
             jokes.append( submission )
-        if "Daniel" in submission.title:
-            send_joke( submission )
 
     joke = random.choice( jokes )
 
     send_joke( joke )
 
 def send_joke( joke ):
-    print( joke, joke.title, joke.selftext )
     send_message( "{}\n\n{}".format( joke.title, joke.selftext ) )
